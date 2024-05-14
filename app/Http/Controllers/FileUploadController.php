f3d2c35649b37337ae660ad8399953527118144d
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-05-14 10:42:02 +0700

    E. Membuat Symlink (Jobsheet 12)

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -43,8 +43,12 @@ class FileUploadController extends Controller
         ]);
         $extFile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extFile;
-        $path = $request->berkas->storeAs('uploads',$namaFile );
+        $path = $request->berkas->storeAs('public',$namaFile );
+
+        $pathBaru=asset('storage/'.$namaFile);
         echo "proses upload berhasil, file berada di: $path";
+        echo "<br>";
+        echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
         // echo $request->berkas-getClientOriginalName(),"lolos validasi";
     }
 }
